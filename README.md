8c1b168c39a4aed1f0812142a0a29866b97512a6
Avesh <aavesh88@gmail.com>
2024-01-02 19:05:52 +0530

    PoW

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Blockchain
 Implementation of simple blockchain in go
 
-Reference: Series of blogs building-blockchain-in-go https://jeiwan.net/posts
+Reference: Series of blogs building-blockchain-in-go https://jeiwan.net
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
 	"strconv"
 	"time"
+
+	"math/big"
 
 	"github.com/google/uuid"
 )
@@ -16,15 +19,7 @@ type Block struct{
 	Hash []byte
 	PrevBlockHash []byte
 	Timestamp int64
-}
-
-func (b *Block) setHash() {
-	timestamp := []byte(strconv.Itoa(int(b.Timestamp)))
-	id := []byte(b.Id)
-	header := bytes.Join([][]byte{id, b.Data, b.PrevBlockHash, timestamp}, []byte(""))
-	hash := sha256.Sum256(header)
-	// Create array to slice
-	b.Hash = hash[:]
+	Nonce         int
 }
 
 func getUUID() string {
@@ -41,7 +36,10 @@ func newBlock(data string, prevBlockHash []byte) *Block{
 	id := getUUID()
 	timestamp := getTimestamp()
 	b := &Block{Id: id, Data: []byte(data), Timestamp: timestamp, PrevBlockHash: prevBlockHash}
-	b.setHash()
+	pow := NewProofOfWork(b)
+	nonce, hash := pow.run()
+	b.Hash = hash
+	b.Nonce = nonce
 	return b
 }
 
@@ -64,6 +62,78 @@ func getBlockchain() *Blockchain{
 	return &Blockchain{blocks: []*Block{genesisBlock()}}
 }
 
+	
+const targetBits = 24
+const maxNonce = math.MaxInt64
+
+type ProofOfWork struct {
+	block *Block
+	target *big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256 - targetBits))
+
+	pow := &ProofOfWork{block: b, target: target}
+	return pow
+}
+
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join([][]byte{
+		pow.block.Data,
+		pow.block.PrevBlockHash,
+		[]byte(pow.block.Id),
+		IntToHex(pow.block.Timestamp),
+		IntToHex(int64(targetBits)),
+		IntToHex(int64(nonce)),
+	}, []byte(""))
+
+	return data
+}
+
+// hardwork
+func (pow *ProofOfWork) run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		// fmt.Printf("\n%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\n")
+
+	return nonce, hash[:]
+}
+
+func IntToHex(data int64) []byte {
+	hex_data := fmt.Sprintf("%x", data)
+	return []byte(hex_data)
+}
+
+func (pow *ProofOfWork) validate() bool {
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isValid := hashInt.Cmp(pow.target) == -1
+	
+	return isValid
+}
+
 func main(){
 	bc := getBlockchain()
 	bc.addBlocks("Sending money to Sachin!")
@@ -75,6 +145,9 @@ func main(){
 		fmt.Println("Timestamp: ", block.Timestamp)
 		fmt.Printf("Hash: %X\n", block.Hash)
 		fmt.Printf("Previous Block Hash: %X\n", block.PrevBlockHash)
+		pow := NewProofOfWork(block)
+		isValid := pow.validate()
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(isValid))
 		fmt.Println()
 	}
 }
